716d6d832d6d3a8d6323b79828c1ab1e047be76d
Donald Simpson <donaldsimpson@gmail.com>
2017-07-15 12:22:42 +0100

    Update basicbuild.groovy

--- basicbuild.groovy
+++ basicbuild.groovy
@@ -1,9 +1,12 @@
-node {
-    stage "Create files"
+node('localhost'){
     
-    writeFile file: "keepthisfile.txt", text: "Some useful info"
-    writeFile file: "ignorethisfile", text: "rubbish"
-
-    stage "Archive files"
-    archiveArtifacts artifacts: 'keep*', excludes: 'ignore*'
+    stage ("Create files"){
+        writeFile file: "keepthisfile.txt", text: "Some useful info"
+        writeFile file: "ignorethisfile", text: "rubbish"
+        sh "docker ps"
+    }
+    
+    stage ("Archive files"){
+        archiveArtifacts artifacts: 'keep*', excludes: 'ignore*'
+    }    
 }
