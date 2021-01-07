e47444a08d9ce1cf5930cf6b6d331e284b62b1cc
Donald Simpson <donaldsimpson@gmail.com>
2021-01-07 10:44:41 +0000

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
+    // See blog post on Jenkins Shared Libraries here: 
+    // https://www.donaldsimpson.co.uk/2019/02/06/jenkins-global-pipeline-libraries-a-v-quick-start-guide/
+    // 
+    // Any valid steps can be called from this code, just like in other Scripted Pipelines
     echo "Hello, ${name}."
 }
